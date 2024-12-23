297a12915046e875dff878f402c7847eee5a289d
manosatpathy <manosatpathy@gmail.com>
2024-12-23 23:33:51 +0530

    feat: initialize project with Next.js setup, navbar, and partial sidebar implementation

---
+++ client/src/app/(components)/Sidebar/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <div>
+      <div className="flex gap-3 justify-between md:justify-normal items-center pt-8">
+        <div>logo</div>
+        <h1 className="font-extrabold text-2xl">MSTOCK</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
